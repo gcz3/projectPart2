a1e1f1fb959013a508a916bcb44295a7e72ff4f7
gcz3 <gcz3@pitt.edu>
2016-11-22 01:37:51 -0500

    linking pages from navbar and adding .htaccess file
    pages now link properly from navbar and now no links need extension
    anymore outside of one that has a weird issue

--- construction/index.php
+++ construction/index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="../css/construction.css">
   </head>
   <body>
-    <?php include('includes/header.php') ?>
+    <?php include('../includes/header.php') ?>
 
     <h1>
       Under Construction! <br>
--- includes/header.php
+++ includes/header.php
@@ -9,46 +9,46 @@
           <span class="icon-bar"></span>
         </button>
 
-        <a href="index.php" class="navbar-brand menu-item">UNIEL[st] ver.3.10 Frame Data</a>
+        <a href="../../projectPart2" class="navbar-brand menu-item">UNIEL[st] ver.3.10 Frame Data</a>
       </div>
 
       <div class="collapse navbar-collapse" id="header-navbar">
         <ul class="nav navbar-nav">
           <li class="nav-item active">
-            <a href="index.php" class="nav-link menu-item">Home <span class="sr-only">(current)</span></a>
+            <a href="../../projectPart2" class="nav-link menu-item">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="dropdown">
             <a href="#" class="dropdown-toggle menu-item" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Characters <span class="caret"></span></a>
             <ul class="dropdown-menu">
-              <li><a href="chara/hyde.php">Hyde</a></li>
-              <li><a href="chara/linne.php">Linne</a></li>
-              <li><a href="chara/waldstein.php">Waldstein</a></li>
-              <li><a href="chara/carmine.php">Carmine</a></li>
-              <li><a href="construction/index.php">Orie</a></li>
-              <li><a href="construction/index.php">Gordeau</a></li>
-              <li><a href="construction/index.php">Merkava</a></li>
-              <li><a href="construction/index.php">Vatista</a></li>
-              <li><a href="construction/index.php">Seth</a></li>
-              <li><a href="construction/index.php">Yuzuriha</a></li>
-              <li><a href="construction/index.php">Hilda</a></li>
-              <li><a href="construction/index.php">Chaos</a></li>
-              <li><a href="construction/index.php">Eltnum</a></li>
-              <li><a href="construction/index.php">Akatsuki</a></li>
-              <li><a href="construction/index.php">Nanase</a></li>
-              <li><a href="construction/index.php">Byakuya</a></li>
-              <li><a href="construction/index.php">Phonon</a></li>
-              <li><a href="construction/index.php">Mika</a></li>
+              <li><a href="../../projectPart2/chara/hyde">Hyde</a></li>
+              <li><a href="../../projectPart2/chara/linne">Linne</a></li>
+              <li><a href="../../projectPart2/chara/waldstein">Waldstein</a></li>
+              <li><a href="../../projectPart2/chara/carmine">Carmine</a></li>
+              <li><a href="../../projectPart2/construction">Orie</a></li>
+              <li><a href="../../projectPart2/construction">Gordeau</a></li>
+              <li><a href="../../projectPart2/construction">Merkava</a></li>
+              <li><a href="../../projectPart2/construction">Vatista</a></li>
+              <li><a href="../../projectPart2/construction">Seth</a></li>
+              <li><a href="../../projectPart2/construction">Yuzuriha</a></li>
+              <li><a href="../../projectPart2/construction">Hilda</a></li>
+              <li><a href="../../projectPart2/construction">Chaos</a></li>
+              <li><a href="../../projectPart2/construction">Eltnum</a></li>
+              <li><a href="../../projectPart2/construction">Akatsuki</a></li>
+              <li><a href="../../projectPart2/construction">Nanase</a></li>
+              <li><a href="../../projectPart2/construction">Byakuya</a></li>
+              <li><a href="../../projectPart2/construction">Phonon</a></li>
+              <li><a href="../../projectPart2/construction">Mika</a></li>
             </ul>
           </li>
           <li class="nav-item">
-            <a href="glossary/index.php" class="nav-link menu-item">Glossary</a>
+            <a href="../../projectPart2/glossary" class="nav-link menu-item">Glossary</a>
           </li>
           <li class="dropdown">
             <a href="#" class="dropdown-toggle menu-item" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About <span class="caret"></span></a>
             <ul class="dropdown-menu">
-              <li><a href="contents/contact.php">Contact Us</a></li>
-              <li><a href="contents/faq.php">About This Site</a></li>
-              <li><a href="contents/link.php">Links</a></li>
+              <li><a href="../../projectPart2/contents/contact.php">Contact Us</a></li> <!-- for some reason, this particular link refuses to work without the .php extension -->
+              <li><a href="../../projectPart2/contents/faq">About This Site</a></li>
+              <li><a href="../../projectPart2/contents/link">Links</a></li>
             </ul>
           </li>
         </ul>
